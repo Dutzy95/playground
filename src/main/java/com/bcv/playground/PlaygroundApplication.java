7ad7660e2b7f987dad601eb3ae42d4addf38d88f
Vlad Bogdan <bogdancristianvlad@yahoo.com>
2023-07-05 02:24:27 +0300

    Added checkstyle. Added suppressions to checkstyle. Added additiona checkstyle rules. Test CI with failing checkstyle

--- src/main/java/com/bcv/playground/PlaygroundApplication.java
+++ src/main/java/com/bcv/playground/PlaygroundApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlaygroundApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(PlaygroundApplication.class, args);
-  }
+	private static String sBla = "bla";
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundApplication.class, args);
+	}
 
 }
